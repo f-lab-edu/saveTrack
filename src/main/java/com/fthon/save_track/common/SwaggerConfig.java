72dd0b0b4a0efcde057d08dd99ec4ea623d6fc3e
minturtle <minseok0425@kakao.com>
2024-09-08 16:35:20 +0900

    feat: Swagger 기본 설정

---
+++ src/main/java/com/fthon/save_track/common/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.fthon.save_track.common;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        String jwtName = "Access Token";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtName);
+        Components components = new Components().addSecuritySchemes(jwtName, new SecurityScheme()
+                .name(jwtName)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+    private Info apiInfo() {
+        return new Info()
+                .title("SaveTrack API Spec.")
+                .description("SaveTrack API 명세서 입니다.")
+                .version("1.0.0");
+    }
+
+}
