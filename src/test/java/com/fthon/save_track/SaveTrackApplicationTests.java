a933d85895b4fa192be51b0e4963d3a099f6d566
minturtle <minseok0425@kakao.com>
2024-09-08 16:21:26 +0900

    feat:Initial Commit

---
+++ src/test/java/com/fthon/save_track/SaveTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fthon.save_track;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
