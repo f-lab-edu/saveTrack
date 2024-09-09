be0e4768c143b1ebbf84f348fc8089a8f0f06483
minturtle <minseok0425@kakao.com>
2024-09-09 17:40:37 +0900

    feat: 카카오 로그인 서비스 로직 개발

--- src/test/java/com/fthon/save_track/auth/service/AuthServiceTest.java
+++ src/test/java/com/fthon/save_track/auth/service/AuthServiceTest.java
@@ -3,6 +3,7 @@ package com.fthon.save_track.auth.service;
 import com.fthon.save_track.auth.dto.KakaoUserInfo;
 import com.fthon.save_track.auth.dto.OAuth2LoginRequest;
 import com.fthon.save_track.auth.service.client.KakaoOAuth2Client;
+import com.fthon.save_track.user.entity.User;
 import com.fthon.save_track.user.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -51,6 +54,34 @@ class AuthServiceTest {
         //then
         assertThat(result).isNotNull();
         assertThat(userRepository.findByKakaoId(kakaoUserInfo.getId()).isPresent()).isTrue();
+    }
+
+
+    @Test
+    @DisplayName("DB에 저장된 사용자의 카카오 토큰을 전달받아 JWT를 반환한다.")
+    public void testSavedKakaoLogin() throws Exception{
+        //given
+        KakaoUserInfo kakaoUserInfo = createDummyKakaoUserInfo();
+        given(kakaoOAuth2Client.getUserInfo(any())).willReturn(kakaoUserInfo);
+
+        User user = new User(
+                kakaoUserInfo.getKakaoAccount().getName(),
+                UUID.randomUUID().toString(),
+                kakaoUserInfo.getId(),
+                kakaoUserInfo.getKakaoAccount().getEmail()
+        );
+        userRepository.save(user);
+
+        //when
+        OAuth2LoginRequest reqBody = new OAuth2LoginRequest(
+                "kakao",
+                "token"
+        );
+
+        String result = authService.doOAuth2Login(reqBody);
+
+        //then
+        assertThat(result).isNotNull();
     }
 
     private KakaoUserInfo createDummyKakaoUserInfo(){
