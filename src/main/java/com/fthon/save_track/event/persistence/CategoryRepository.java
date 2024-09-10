24084626ad6dfcafc1dc27ff74928b0245a9ce3c
capDoYeonLee <ghtyu21@naver.com>
2024-09-10 14:30:36 +0900

    feat: api 호출 중복 방지

--- src/main/java/com/fthon/save_track/event/persistence/CategoryRepository.java
+++ src/main/java/com/fthon/save_track/event/persistence/CategoryRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.stream.Stream;
 
 @Repository
@@ -12,5 +13,6 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
     @Query("select c from Category c")
     Stream<Category> getList();
 
+    Optional<Category> findByName(String name);
 
 }
