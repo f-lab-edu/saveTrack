c262e281e8d26069e558d9a9161a3e24057e9fa8
minturtle <minseok0425@kakao.com>
2024-09-11 14:33:44 +0900

    refactor : OAuth2 Swagger 문구 변경

--- src/main/java/com/fthon/save_track/auth/controller/AuthController.java
+++ src/main/java/com/fthon/save_track/auth/controller/AuthController.java
@@ -23,7 +23,7 @@ public class AuthController {
     private final AuthService authService;
 
     @Operation(summary = "OAuth2 로그인", description = "OAuth2 로그인을 처리합니다.")
-    @ApiResponse(responseCode = "200", description = "성공적으로 로그인된 경우로, Access Token은 Authorization 헤더에 담겨서 전달됩니다.")
+    @ApiResponse(responseCode = "200", description = "성공적으로 로그인된 경우로, Access Token은 Response Body에 담겨서 전달됩니다.")
     @ApiResponse(responseCode = "400", description = "잘못된 요청",
             content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     @PostMapping("/oauth")
