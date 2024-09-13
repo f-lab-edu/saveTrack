9d35b9d190954ae0ec2ffd07ce904c8f3644462a
capDoYeonLee <ghtyu21@naver.com>
2024-09-13 22:56:03 +0900

    feat: 이벤트 조회 query 추가

--- src/main/java/com/fthon/save_track/event/persistence/EventRepository.java
+++ src/main/java/com/fthon/save_track/event/persistence/EventRepository.java
@@ -1,11 +1,15 @@
 package com.fthon.save_track.event.persistence;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
@@ -14,4 +18,10 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     @Transactional
     @Query("UPDATE Event e SET e.deleted = true WHERE e.id = :eventId")
     void softDeleteEvent(@Param("eventId") Long eventId);
+
+    Page<Event> findAll(Pageable pageable);
+
+    @Query("SELECT e FROM Event e WHERE e.category.id = :categoryId")
+    Page<Event> findByCategoryId(Pageable pageable, @Param("categoryId") Long categoryId);
+
 }
