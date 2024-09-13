c8a41fe9362eaed14d7fb6d53db20f664002755c
capDoYeonLee <ghtyu21@naver.com>
2024-09-13 22:16:07 +0900

    feat: soft delete query 추가

--- src/main/java/com/fthon/save_track/event/persistence/EventRepository.java
+++ src/main/java/com/fthon/save_track/event/persistence/EventRepository.java
@@ -1,9 +1,17 @@
 package com.fthon.save_track.event.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
 
+    @Modifying
+    @Transactional
+    @Query("UPDATE Event e SET e.deleted = true WHERE e.id = :eventId")
+    void softDeleteEvent(@Param("eventId") Long eventId);
 }
